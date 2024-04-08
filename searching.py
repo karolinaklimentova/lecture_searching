0cedcde80b0a6f02dd9c2d67add9bbdd16652b82
Karolina Klimentova <257007@vutbr.cz>
2024-04-08 12:30:00 +0200

    Add function for linear search

--- searching.py
+++ searching.py
@@ -32,10 +32,31 @@ def read_data(file_name, field):
     return data[field]
 
 
+def linear_search(sequence, target):
+    """
+    Performs linear search on an unordered sequence to find positions and count of a target number.
+    :param sequence: (list) unordered sequence to search
+    :param target: (int) target number to search for
+    :return: (dict) dictionary containing positions and count of the target number
+    """
+    positions = []
+    count = 0
+    for index, number in enumerate(sequence):
+        if number == target:
+            positions.append(index)
+            count += 1
+    return {"positions": positions, "count": count}
+
+
 def main():
     sequential_data = read_data("sequential.json", "unordered_numbers")
 
     print("Sequential data:", sequential_data)
+
+    target_number = int(input("Enter the number to search for: "))
+
+    result = linear_search(sequential_data, target_number)
+    print("Result:", result)
 
 
 if __name__ == '__main__':
