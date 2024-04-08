9ff8cb81b5f025ea55effa4a84eb0983c35059d0
Karolina Klimentova <257007@vutbr.cz>
2024-04-08 12:17:27 +0200

    Add read data function

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -13,9 +14,28 @@ def read_data(file_name, field):
     """
     file_path = os.path.join(cwd_path, file_name)
 
+    if not os.path.exists(file_path):
+        print(f"File {file_name} not found.")
+        return None
+
+    with open(file_path, "r") as file:
+        try:
+            data = json.load(file)
+        except json.JSONDecodeError:
+            print(f"Error decoding JSON from file {file_name}.")
+            return None
+
+    if field not in data:
+        print(f"Field '{field}' not found in {file_name}")
+        return None
+
+    return data[field]
+
 
 def main():
-    pass
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+
+    print("Sequential data:", sequential_data)
 
 
 if __name__ == '__main__':
